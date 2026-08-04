51cdc37db5eea7f59bdd22947e629298124bd815
SadanandJaiswal <sadanandjaiswal92@gmail.com>
2026-08-04 22:13:06 +0530

    Solved Kth Largest in a Stream

---
+++ Heap/11_KthLargest_Elm_in_a_Stream.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class KthLargest {
+private:
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int k;
+public:
+    KthLargest(int k, vector<int>& nums) {
+
+        this->k = k;
+
+        for(int it : nums){
+            if(pq.size()<k){
+                pq.push(it);
+            }else if(it>pq.top()){
+                pq.pop();
+                pq.push(it);
+            }
+        }
+    }
+    
+    int add(int val) {
+        if(pq.size()<k){
+            pq.push(val);
+        }else if(val>pq.top()){
+            pq.pop();
+            pq.push(val);
+        }
+
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
