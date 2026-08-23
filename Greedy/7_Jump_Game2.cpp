5dc940be4e5f14aa6aca7b51b963bb410c034c39
SadanandJaiswal <sadanandjaiswal92@gmail.com>
2026-08-23 23:45:51 +0530

    Solved GFG Minimum Jumps

--- Greedy/7_Jump_Game2.cpp
+++ Greedy/7_Jump_Game2.cpp
@@ -80,3 +80,35 @@ public:
 };
 // Time Complexity : O(n);
 // Space Complexity : O(1);
+
+
+class SolutionGFGMinimumJumps {
+  public:
+    int minJumps(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        
+        if(n<=1) return 0;
+        
+        if(arr[0]==0) return -1;
+        
+        int jumps = 0;
+        int l=0, r=0;
+        
+        while(r<n-1){
+            int farthest = r;
+            
+            for(int i=l; i<=r; i++){
+                farthest = max(farthest, i+arr[i]);
+                l++;
+            }
+            
+            if(farthest==r) return -1;
+            
+            r = farthest;
+            jumps++;
+        }
+        
+        return jumps;
+    }
+};
